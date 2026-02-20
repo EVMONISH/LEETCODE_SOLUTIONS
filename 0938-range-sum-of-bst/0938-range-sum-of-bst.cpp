ea66dbca18fe0d797486e14866a68063bea88b03
EVMONISH <149870690+EVMONISH@users.noreply.github.com>
2026-02-20 10:30:56 +0530

    Time: 0 ms (100%), Space: 64.9 MB (61.46%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root == NULL) return 0;
+        if(root->val < low) return rangeSumBST(root->right,low,high);
+        if(root->val > high) return rangeSumBST(root->left,low,high);
+        return root->val + rangeSumBST(root->right,low,high) + rangeSumBST(root->left,low,high);
+    }
+};
+
+
+
+
+        //iterative approach
+
+// class Solution {
+// public:
+//     int rangeSumBST(TreeNode* root, int low, int high) {
+//         if(root == NULL) return 0;
+        
+//         int sum=0;
+//         stack<TreeNode*> st;
+//         st.push(root);
+
+//         while(!st.empty())
+//         {
+//             TreeNode *node = st.top();
+//             st.pop();
+
+//             if(node == NULL)
+//                 continue;
+                
+//             if(node->val >= low && node->val <= high)
+//             {
+//                 sum += node->val;
+//             }
+            
+//             if(node->val > low)
+//                 st.push(node->left);
+            
+//             if(node->val < high)
+//                 st.push(node->right);
+//         }
+//         return sum;
+//     }
+// };
