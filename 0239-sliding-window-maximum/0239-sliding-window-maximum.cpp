9e3abac4acea67f91489349e9b13e4d10fcf7750
EVMONISH <149870690+EVMONISH@users.noreply.github.com>
2026-03-30 18:49:14 +0530

    Time: 23 ms (59.71%), Space: 139.3 MB (25.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,54 @@
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         map<int,int>mpp;
+//         vector<int> ans;
+
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             mpp[nums[i]]++;
+
+//             if(i>=k)
+//             {
+//                 mpp[nums[i-k]]--;
+//                 if(mpp[nums[i-k]] == 0)
+//                     mpp.erase(nums[i-k]);
+//             }
+
+//             if(i>=k-1)
+//                 ans.push_back(mpp.rbegin()->first);
+//         }
+//         return ans;
+//     }
+// };
+
+
+        //using stack and queues
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        map<int,int>mpp;
-        vector<int> ans;
+        deque<int> dq;
+        vector<int> result;
+        int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++)
+        for(int i=0; i<n; i++)
         {
-            mpp[nums[i]]++;
-
-            if(i>=k)
+            if(!dq.empty() && dq.front() <= i-k)
             {
-                mpp[nums[i-k]]--;
-                if(mpp[nums[i-k]] == 0)
-                    mpp.erase(nums[i-k]);
+                dq.pop_front();
             }
+            while(!dq.empty() && nums[dq.back()] < nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
 
-            if(i>=k-1)
-                ans.push_back(mpp.rbegin()->first);
+            if(i >= k-1)
+            {
+                result.push_back(nums[dq.front()]);
+            }
         }
-        return ans;
+        return result;
     }
-
 };
